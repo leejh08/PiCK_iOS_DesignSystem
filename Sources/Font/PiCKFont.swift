9462be29ed41a768ba35ec3b51e315b7256d0fe5
jihoon <leejh080628@naver.com>
2025-09-28 01:04:15 +0900

    ✨ :: 명시적 타입 수정

--- Sources/Font/PiCKFont.swift
+++ Sources/Font/PiCKFont.swift
@@ -1,58 +1,47 @@
 import SwiftUI
+import UIKit
 
-public extension Font {
-    static func pickFont(_ font: PiCKFontStyle) -> Font {
-        return .custom(font.uiFont().fontName, size: font.size())
+public extension SwiftUI.Font {
+    static func pickFont(_ font: PiCKFontStyle) -> SwiftUI.Font {
+        let uiFont = font.uiFont()
+        return SwiftUI.Font.custom(uiFont.fontName, size: font.size())
     }
 }
 
 public extension PiCKFontStyle {
-    func uiFont() -> UIFont {
+    internal func uiFont() -> UIFont {
         let wantedSans = DesignSystemFontFamily.WantedSans.self
 
         switch self {
-
         case .heading1, .heading2, .heading3, .heading4, .subTitle1, .subTitle2, .subTitle3, .button1, .button2:
             return wantedSans.semiBold.font(size: self.size())
-
         case .body1, .body2, .body3, .label1, .label2:
             return wantedSans.medium.font(size: self.size())
-
         case .caption1, .caption2:
             return wantedSans.regular.font(size: self.size())
         }
     }
 
-    func size() -> CGFloat {
+    public func size() -> CGFloat {
         switch self {
-
         case .heading1:
             return 42
-
         case .heading2:
             return 32
-
         case .heading3:
             return 24
-
         case .heading4:
             return 20
-
         case .subTitle1:
             return 18
-
         case .subTitle2, .caption1, .label1, .button1:
             return 16
-
         case .subTitle3, .body1:
             return 14
-
         case .body2, .caption2, .label2, .button2:
             return 12
-
         case .body3:
             return 10
         }
     }
-
 }
