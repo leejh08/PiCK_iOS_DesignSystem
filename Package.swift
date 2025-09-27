7f4ff737b128e6a06bc5ccebcecbd44c9b206b0a
jihoon <leejh080628@naver.com>
2025-09-28 00:53:05 +0900

    ✨ :: 단일 모듈 컴파일

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "PiCK_iOS_DesignSystem",
-            resources: [.process("Resources")]
+            path: "Sources"
         ),
 
     ]
