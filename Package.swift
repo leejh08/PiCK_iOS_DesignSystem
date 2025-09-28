bf04e996f039f850ce14bd4316a73bd0e0957345
jihoon <leejh080628@naver.com>
2025-09-28 15:11:26 +0900

    ✨ :: 경로 수정

--- Package.swift
+++ Package.swift
@@ -1,26 +1,22 @@
-// swift-tools-version: 6.2
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "PiCK_iOS_DesignSystem",
-    platforms: [.iOS(.v13)],
+    platforms: [
+        .iOS(.v15)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "PiCK_iOS_DesignSystem",
-            targets: ["PiCK_iOS_DesignSystem"]
-        ),
+            targets: ["PiCK_iOS_DesignSystem"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "PiCK_iOS_DesignSystem",
-            path: "Sources",
-            resources: [.process("PiCK_iOS_DesignSystem/Resources")]
-        ),
-
+            path: "PiCK_iOS_DesignSystem"
+        )
     ]
 )
--- Sources/PiCK_iOS_DesignSystem/DesignSystemAsset.swift
+++
@@ -1,6 +0,0 @@
-import Foundation
-
-public enum DesignSystemAsset {
-    public typealias Color = ColorAsset
-    public typealias Image = ImageAsset
-}
